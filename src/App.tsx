b8623ca99299a20c0e70d068ca0455ba2519e7ca
Nils <78668364+Suchti18@users.noreply.github.com>
2025-07-07 21:42:45 +0200

    Complete layout template

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ function App() {
   return (
     <>
       <div className="controller">
-          <h1>Controller</h1>
           <div className="left">
               <div className="jogwheel">
 
@@ -29,13 +28,51 @@ function App() {
                       <div></div>
                       <div></div>
                   </div>
+                  <div className="tempoSlider">
+
+                  </div>
               </div>
           </div>
           <div className="center">
-              <h1>Center</h1>
+              <div className="mixer">
+                  <div className="channel">
+
+                  </div>
+                  <div className="channel">
+
+                  </div>
+              </div>
+              <div className="positionSlider">
+
+              </div>
           </div>
           <div className="right">
-              <h1>Right</h1>
+              <div className="jogwheel">
+
+              </div>
+              <div className="controls">
+                  <div className="startMusicButtons">
+                      <div className="queButton">
+
+                      </div>
+                      <div className="startButton">
+
+                      </div>
+                  </div>
+                  <div className="fxButtons">
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                  </div>
+                  <div className="tempoSlider">
+
+                  </div>
+              </div>
           </div>
       </div>
     </>
